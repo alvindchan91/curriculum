f0e1707e6e879180820ae43d456dc3f775d4bf3c
Alvin Chan <alvindchan91@gmail.com>
2018-03-06 22:50:22 -0800

    js1-8

--- curriculum/js1/8.js
+++ curriculum/js1/8.js
@@ -5,9 +5,10 @@
  * @returns {number}
  */
 
-const solution = (a, b) => {
-  return 0;
-};
+const solution = (a, b, i=Math.max(a,b)) => {
+  if (a%i===0 && b%i===0) return i;
+  return solution(a, b, i-1)
+}
 
 module.exports = {
   solution,
